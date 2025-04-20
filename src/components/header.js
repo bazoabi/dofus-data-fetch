45d08694ddce6a67c67ae032fd5738914a4ea2fa
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-21 01:58:30 +0200

    Add Equipment Search functionality using Combo Box by MUI

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,8 @@ import dofusLogo from "../assets/dofus-logo.png";
 export default function Header() {
   // resize the image to 100x100 pixels
   const style = {
-    width: "100%",
-    height: "100%",
+    width: "70%",
+    height: "70%",
   };
 
   return <img style={style} src={dofusLogo} alt="Dofus Logo" />;
