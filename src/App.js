9b97624312179ce92774f45a65759f760f3448e5
Bader Zoabi <bader.zoabi@gmail.com>
2025-04-21 03:45:03 +0200

    Create Item Card and show the selected equipment data

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
+
 // Components
 import Header from "./components/header";
 
@@ -19,9 +29,12 @@ import axios from "axios";
 let cancelAxios = null;
 
 function App() {
+  const [allEquipmentData, setAllEquipmentData] = useState([]);
   const [equipmentNames, setEquipmentNames] = useState([]);
-  const [selectedEquipment, setSelectedEquipment] = useState("");
-  const [selectedInputEquipment, setSelectedInputEquipment] = useState("");
+  const [selectedEquipmentName, setSelectedEquipmentName] = useState("");
+  const [selectedInputEquipmentName, setSelectedInputEquipmentName] =
+    useState("");
+  const [selectedEquipmentData, setSelectedEquipmentData] = useState(null);
 
   // Fetch data from the API
   useEffect(() => {
@@ -44,6 +57,8 @@ function App() {
         // handle success
         console.log("success: ", response);
 
+        setAllEquipmentData(response.data.items);
+        console.log("Equipment Data: ", response.data.items);
         // Create an array of all the names of the equipment
         const equipmentNamesResponse = response.data.items.map(
           (item) => item.name
@@ -79,7 +94,7 @@ function App() {
             color: "white",
           }}
         >
-          {selectedEquipment}
+          {selectedEquipmentName}
         </div>
         {/* ==== Show Selected Equipment Name Label ==== */}
 
@@ -91,13 +106,16 @@ function App() {
           }}
         >
           <Autocomplete
-            value={selectedEquipment ?? ""}
+            value={selectedEquipmentName ?? ""}
             onChange={(event, newValue) => {
-              setSelectedEquipment(newValue);
+              setSelectedEquipmentName(newValue);
             }}
-            inputValue={selectedInputEquipment}
+            inputValue={selectedInputEquipmentName}
             onInputChange={(event, newInputValue) => {
-              setSelectedInputEquipment(newInputValue);
+              setSelectedInputEquipmentName(newInputValue);
+              setSelectedEquipmentData(
+                allEquipmentData.find((item) => item.name === newInputValue)
+              );
             }}
             disablePortal
             options={equipmentNames}
@@ -114,12 +132,80 @@ function App() {
             renderInput={(params) => (
               <TextField
                 {...params}
-                label={selectedEquipment === "" ? "Equipment" : ""}
+                label={selectedEquipmentName === "" ? "Equipment" : ""}
               />
             )}
           />
         </div>
         {/* ==== Combo Box Search Section ==== */}
+
+        {/* Item Card */}
+        <Box
+          sx={{
+            // minWidth: 275,
+            width: "100%",
+            marginTop: "20px",
+            boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.5)",
+          }}
+        >
+          <Card
+            variant="outlined"
+            sx={{ backgroundColor: "#1a1a1a", color: "white" }}
+          >
+            <CardContent>
+              <div>
+                <Typography
+                  gutterBottom
+                  sx={{ fontSize: 14, color: "lightcyan" }}
+                >
+                  {selectedEquipmentData?.name}
+                </Typography>
+                <Typography gutterBottom sx={{ color: "red", fontSize: 12 }}>
+                  {selectedEquipmentData?.type?.name}
+                </Typography>
+                <div>
+                  <img
+                    src={selectedEquipmentData?.image_urls.sd}
+                    alt={selectedEquipmentData?.name}
+                    style={{ width: "100px", height: "100px" }}
+                  />
+                </div>
+              </div>
+              <div>
+                <div
+                  // variant="body2"
+                  sx={{
+                    // columnCount: 2, // how many columns
+                    columnGap: 2, // space between columns (theme units)
+                    // optional: balance content better
+                    // columnFill: "auto",
+                  }}
+                >
+                  {selectedEquipmentData?.effects.map((effect) => (
+                    <Typography
+                      variant="body2"
+                      key={effect.type.id}
+                      style={{
+                        color:
+                          String(effect.formatted).length > 50
+                            ? "white"
+                            : String(effect.formatted).includes("-")
+                            ? "lightcoral"
+                            : "lightgreen",
+                      }}
+                    >
+                      {effect.formatted}
+                    </Typography>
+                  ))}
+                </div>
+              </div>
+            </CardContent>
+            {/* <CardActions>
+              <Button size="small">Learn More</Button>
+            </CardActions> */}
+          </Card>
+        </Box>
+        {/* ==== Item Card ==== */}
       </Container>
     </div>
   );
